4a13750a5fdfab66090ab5e57afcd08047fb1c96
unknown <muh.it.eng.19841984@gmail.com>
2022-11-20 12:34:48 +0200

    docker node with a new 2 services mong,mongo-express

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const PORT = process.env.PORT || 4000;
 
 // init app
 const app = express();
 
+// connect MongoDB
+const DB_USER = "root";
+const DB_PASSWORD = "example";
+const DB_PORT = 27017;
+const DB_HOST = "mongo"; // name of service in docker-commpose
+
+const URI = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}`;
+
+mongoose
+  .connect(URI)
+  .then(() => console.log("connected to mongodb : successfully"))
+  .catch((err) => console.log(err));
+
+// Routes
 app.get("/", (req, res) => {
   res.send("<h1>Hello</h1>");
 });
